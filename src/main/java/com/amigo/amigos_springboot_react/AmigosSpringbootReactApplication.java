ab408e044894cd6a3164fc4254484b67eb39a175
Zalyalov-Almaz <zalyalov.almaz2016@gmail.com>
2023-04-25 16:36:31 +0300

    [01] - initial commit

---
+++ src/main/java/com/amigo/amigos_springboot_react/AmigosSpringbootReactApplication.java
@@ -0,0 +1,13 @@
+package com.amigo.amigos_springboot_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmigosSpringbootReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmigosSpringbootReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amigo/amigos_springboot_react/AmigosSpringbootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amigo.amigos_springboot_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigosSpringbootReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
